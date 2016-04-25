e26517183fd1876a99777dbaedf06c8ae30ea4e4
Christian McFarland <Christian McFarland>
2016-04-25 12:44:01 -0700

    added some styling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $.getJSON('topspots.json', function(data){
 	$.each(data, function(i, spots){
-		var myLink = "<a href=https://www.google.com/maps?q=" + spots.location + ">Location</a>"
+		var myLink = "<a href=https://www.google.com/maps?q=" + spots.location + " class='btn btn-default' target='_blank'>Go!</a>";
 		$('table').append('<tr><td>' + spots.name + '</td><td>' + spots.description + '</td><td>'+ myLink +'</td></tr>');
 	})
 });
